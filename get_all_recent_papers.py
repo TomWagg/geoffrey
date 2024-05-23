1c6f4b7c95a65547c43e131619c91abb3586e369
Tom Wagg <tomjwagg@gmail.com>
2024-05-23 14:12:23 -0700

    quick script for getting recent papers

---
+++ get_all_recent_papers.py
@@ -0,0 +1,16 @@
+import ads_query
+import datetime
+import pandas as pd
+
+orcids = pd.read_csv("data/orcids.csv")["orcid"].values
+last_names = pd.read_csv("data/orcids.csv")["last_name"].values
+
+for orcid, last_name in zip(orcids, last_names):
+    papers_dict_list = ads_query.get_ads_papers(query=f"orcid:{orcid}", past_week=False,
+                                                remove_known_papers=True)
+
+    recent_papers = [paper for paper in papers_dict_list if paper["date"] > datetime.date(2023, 8, 1)]
+
+    print(f"Found {len(recent_papers)} papers for {last_name}")
+
+    ads_query.save_papers(recent_papers)
